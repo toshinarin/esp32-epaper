2e8be02b64579e00dda82c4abc75492e39a39c51
toshinarin <toshinarin@gmail.com>
2018-04-01 12:26:11 +0900

    Receive empty value

--- BLE_uart.cpp
+++ BLE_uart.cpp
@@ -24,18 +24,15 @@ MyCallbacks::MyCallbacks(BLEWrittenCallback cb) {
 void MyCallbacks::onWrite(BLECharacteristic *pCharacteristic) {
   std::string rxValue = pCharacteristic->getValue();
 
-  if (rxValue.length() > 0) {
-    Serial.println("*********");
-    Serial.print("Received Value: ");
-    for (int i = 0; i < rxValue.length(); i++)
-      Serial.print(rxValue[i]);
+  Serial.println("*********");
+  Serial.print("Received Value: ");
+  for (int i = 0; i < rxValue.length(); i++)
+    Serial.print(rxValue[i]);
 
-    Serial.println();
-    Serial.println("*********");
+  Serial.println();
+  Serial.println("*********");
 
-    callback(rxValue.c_str());
-
-  }
+  callback(rxValue.c_str());
 }
 
 BLEUart::BLEUart(BLEWrittenCallback cb) {
