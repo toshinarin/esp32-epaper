99f0a2b707973730d7e75f511dec286c243b159e
toshinarin <toshinarin@gmail.com>
2018-04-01 11:41:09 +0900

    Remove unneeded code

--- esp32_epaper.ino
+++ esp32_epaper.ino
@@ -68,28 +68,6 @@
 #include <GxIO/GxIO_SPI/GxIO_SPI.cpp>
 #include <GxIO/GxIO.cpp>
 
-#if defined(ESP8266)
-
-// generic/common.h
-//static const uint8_t SS    = 15;
-//static const uint8_t MOSI  = 13;
-//static const uint8_t MISO  = 12;
-//static const uint8_t SCK   = 14;
-// pins_arduino.h
-//static const uint8_t D8   = 15;
-//static const uint8_t D7   = 13;
-//static const uint8_t D6   = 12;
-//static const uint8_t D5   = 14;
-
-// GxIO_SPI(SPIClass& spi, int8_t cs, int8_t dc, int8_t rst = -1, int8_t bl = -1);
-GxIO_Class io(SPI, SS, D3, D4); // arbitrary selection of D3, D4 selected for default of GxEPD_Class
-// GxGDEP015OC1(GxIO& io, uint8_t rst = D4, uint8_t busy = D2);
-GxEPD_Class display(io); // default selection of D4, D2
-// my IoT connection, busy on MISO
-//GxEPD_Class display(io, D4, D6);
-
-#elif defined(ESP32)
-
 // pins_arduino.h, e.g. LOLIN32
 //static const uint8_t SS    = 5;
 //static const uint8_t MOSI  = 23;
@@ -100,64 +78,6 @@ GxEPD_Class display(io); // default selection of D4, D2
 GxIO_Class io(SPI, SS, 17, 16); // arbitrary selection of 17, 16
 // GxGDEP015OC1(GxIO& io, uint8_t rst = D4, uint8_t busy = D2);
 GxEPD_Class display(io, 16, 4); // arbitrary selection of (16), 4
-
-#elif defined(ARDUINO_ARCH_SAMD)
-
-// variant.h of MKR1000
-//#define PIN_SPI_MISO  (10u)
-//#define PIN_SPI_MOSI  (8u)
-//#define PIN_SPI_SCK   (9u)
-//#define PIN_SPI_SS    (24u) // should be 4?
-// variant.h of MKRZERO
-//#define PIN_SPI_MISO  (10u)
-//#define PIN_SPI_MOSI  (8u)
-//#define PIN_SPI_SCK   (9u)
-//#define PIN_SPI_SS    (4u)
-
-GxIO_Class io(SPI, 4, 7, 6);
-GxEPD_Class display(io, 6, 5);
-
-#elif defined(_BOARD_GENERIC_STM32F103C_H_)
-
-// STM32 Boards (STM32duino.com)
-// Generic STM32F103C series
-// aka BluePill
-// board.h
-//#define BOARD_SPI1_NSS_PIN        PA4
-//#define BOARD_SPI1_MOSI_PIN       PA7
-//#define BOARD_SPI1_MISO_PIN       PA6
-//#define BOARD_SPI1_SCK_PIN        PA5
-//enum {
-//    PA0, PA1, PA2, PA3, PA4, PA5, PA6, PA7, PA8, PA9, PA10, PA11, PA12, PA13,PA14,PA15,
-//  PB0, PB1, PB2, PB3, PB4, PB5, PB6, PB7, PB8, PB9, PB10, PB11, PB12, PB13,PB14,PB15,
-//  PC13, PC14,PC15
-//};
-// variant.h
-//static const uint8_t SS   = BOARD_SPI1_NSS_PIN;
-//static const uint8_t SS1  = BOARD_SPI2_NSS_PIN;
-//static const uint8_t MOSI = BOARD_SPI1_MOSI_PIN;
-//static const uint8_t MISO = BOARD_SPI1_MISO_PIN;
-//static const uint8_t SCK  = BOARD_SPI1_SCK_PIN;
-
-// GxIO_SPI(SPIClass& spi, int8_t cs, int8_t dc, int8_t rst = -1, int8_t bl = -1);
-GxIO_Class io(SPI, SS, 8, 9);
-// GxGDEP015OC1(GxIO& io, uint8_t rst = 9, uint8_t busy = 7);
-GxEPD_Class display(io, 16, 4);
-
-#else
-
-// pins_arduino.h, e.g. AVR
-//#define PIN_SPI_SS    (10)
-//#define PIN_SPI_MOSI  (11)
-//#define PIN_SPI_MISO  (12)
-//#define PIN_SPI_SCK   (13)
-
-GxIO_Class io(SPI, SS, 17, 16); // arbitrary selection of 8, 9 selected for default of GxEPD_Class
-//GxIO_DESTM32L io;
-//GxIO_GreenSTM32F103V io;
-GxEPD_Class display(io);
-
-#endif
 
 #include "BLE_uart.h"
 
